bfc87171c7747e3982d7a139f90959a67f202dd7
Yuto Oshiro <e195732@ie.u-ryukyu.ac.jp>
2019-12-23 01:50:09 +0900

    code of step1

---
+++ src/main/java/jp/ac/uryukyu/ie/e195732/LivingThing.java
@@ -0,0 +1,87 @@
+package jp.ac.uryukyu.ie.e195732;
+
+/**
+ * 生物クラス。
+ *  String name; //生物の名前
+ *  int hitPoint; //生物のHP
+ *  int attack; //生物の攻撃力
+ *  boolean dead; //生物の生死状態。true=死亡。
+ * Created by Yuto on 2019/11/23.
+ */
+public class LivingThing {
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+
+    public LivingThing(String name, int hitPoint, int attack){
+        this.name = name;
+        this.hitPoint = hitPoint;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitPoint, attack);
+        }
+
+    /**
+     * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
+     * @return boolean
+     */
+
+    public boolean isDead(){
+        return dead;
+    }
+
+    public void setDead(boolean dead) {
+        this.dead = dead;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getHitPoint() {
+        return hitPoint;
+    }
+
+    public void setHitPoint(int hitPoint) {
+        this.hitPoint = hitPoint;
+    }
+
+    /**
+     * Heroへ攻撃するメソッド。
+     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * @param opponent 攻撃対象
+     */
+    public void attack(LivingThing opponent){
+        if (!isDead()){
+            int damage = (int)(Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("%sは倒れた。\n", name);
+        }
+    }
+}
---
+++ src/test/java/jp/ac/uryukyu/ie/e195732/EnemyTest.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e195732;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTest {
+
+    @Test
+    void wounded() {
+    }
+}
